dccf19859bd95126a42901dbdf35e4a9e9477d51
Steven Ruiz <ruiz.steven.j@gmail.com>
2019-01-29 21:03:23 -0700

    finish exercise 2 from the variable chapter

---
+++ exercise_2.rb
@@ -0,0 +1,30 @@
+# 2. What do the follwing expressions evaluate to?
+
+1. x = 2
+# returns => 2
+2. puts x = 2
+# returns 2
+# => nil
+3. p name = "Joe"
+# returns "Joe"
+# => "Joe"
+4. four = "four"
+# retuns => "four"
+5. print something = "nothing"
+# returns nothing => nil
+# Below is the irb resolution of the questions for exercise number 2
+
+=begin
+2.6.0 :001 > x = 2
+ => 2
+2.6.0 :002 > puts x = 2
+2
+ => nil
+2.6.0 :003 > p name = "Joe"
+"Joe"
+ => "Joe"
+2.6.0 :004 > four = "four"
+ => "four"
+2.6.0 :005 > print something = "nothing"
+nothing => nil
+=end
